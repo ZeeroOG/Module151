e08a23f07e835c32794f589e2eaedffb5003d2c5
William Maillard <william@zeero.ch>
2016-11-18 17:13:53 +0100

    création du routeur

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php
 
-echo "Hello World !";
+// ========================= //
+// Système de routage génial //
+// ========================= //
+
+$pages = array();
+
+$pages['home'] = 'app/controler/home.php';
+$pages['test'] = 'app/controler/test.php';
+
+if(!isset($_GET['p']) || !array_key_exists($_GET['p'], $pages)) {
+    header('Location: ?p=home');
+    die();
+} else {
+    $page = $_GET['p'];
+}
+
+$page_link = $pages[$page];
 
 ?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>routeur</title>
+</head>
+<body>
+<?php
+
+include($page_link);
+echo "\n";
+
+?>
+</body>
+</html>
