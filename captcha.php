5ca6c1a4328a83e4ecd37618c7db1f9353159916
jkghsdjkguig <iguuigg@sdfofhsod.ch>
2016-11-23 13:30:32 +0100

    captcha sa mère

--- captcha.php
+++ captcha.php
@@ -9,28 +9,39 @@ class Captcha {
 
 	public function __construct()
 	{
+		// Générer le Captcha
 		$this->generate();
+	}
+
+	private function getTextColor()
+	{
+		// Retourne une des couleurs RGB (aléatoire)
+		switch (rand(0, 2)) {
+			case 0:
+				return array(0, 100, 200);
+				break;
+			case 1:
+				return array(200, 0, 0);
+				break;
+		  case 2:
+				return array(102, 0, 102);
+				break;
+		}
 	}
 
 	private function generate()
 	{
+		// Creation de l'image
 		$image = imagecreate(300, 75);
+
+		// Couleur de fonds de l'image
 		imagecolorallocate($image, 222, 222, 222);
 
-		switch (rand(0, 2)) {
-			case 0:
-				$text_color = imagecolorallocate($image, 0, 100, 200);
-				break;
-			case 1:
-				$text_color = imagecolorallocate($image, 200, 0, 0);
-				break;
-		  case 2:
-				$text_color = imagecolorallocate($image, 102, 0, 102);
-				break;
-		}
+		// Choisir la couleur du texte aléatoirement
+		$text_rgb = $this->getTextColor();
+		$text_color = imagecolorallocate($image, $text_rgb[0], $text_rgb[1], $text_rgb[2]);
 
-		$line_color = imagecolorallocate($image, 64, 64, 64);
-
+		// Générer un code aléatoire
 		for($i = 0; $i < 6; $i++) {
 			if(rand(0, 1) == 0) $x = chr(rand(65,90));
 			else $x = rand(0, 9);
@@ -39,22 +50,30 @@ class Captcha {
 			$text = $text . $x . " ";
 		}
 
-		imagettftext($image, 34, -rand(1, 5), 20, 40, $text_color, "font.ttf", $text);
+		// Affichage du texte (avec un angle aléatoire entre -1 et -5 deg)
+		imagettftext($image, 34, -rand(1, 5), 20, 40, $text_color, "captcha.ttf", $text);
 
+		// Couleur des lignes
+		$line_color = imagecolorallocate($image, 64, 64, 64);
+
+		// Ajouter 8 lignes
 		for($i = 0; $i < 7; $i++) {
 			imageline($image, 0, rand()%50, 300, rand()%50, $line_color);
 		}
 
+		// Transformation de l'image en base64
 		ob_start ();
 		imagepng($image);
 		$image_data = ob_get_contents();
 		ob_end_clean ();
 
+		// Retourner le captcha en image (base64) et en plaintext
 		$this->texte = strtolower($code);
 		$this->image = "data:image/png;base64," . base64_encode($image_data);
 	}
 }
 
+// TEST
 $captcha = new Captcha();
 
 ?>
@@ -62,7 +81,7 @@ $captcha = new Captcha();
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title></title>
+		<title>Test Captcha</title>
 	</head>
 	<body>
 		<img src="<?php echo $captcha->getImage(); ?>" />
