02ac69d52ea38bce2ab39c70a96322351faed160
bukibarak <bukibarak@gmail.com>
2016-11-25 15:14:25 +0100

    Ajout view pour inserer les film dans la DB

---
+++ app/view/DBmanagement.php
@@ -0,0 +1,5 @@
+<form action="." method="SET" accept-charset="UTF-8">
+	<label for="titreoriginal">Titre: </label><input type="text" id="titreoriginal" name="titreoriginal" placeholder="Titre original du film" autofocus required/>
+	<label for="titretraduit">Titre traduit (facultatif): </label><input type="text" id="titretraduit" name="titretraduit" placeholder="Titre du film traduit" autofocus/>
+	<label for="duree">Durée: </label>
+	
