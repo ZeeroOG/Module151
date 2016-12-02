87a566793764b62712ddb5e8258123c7528fbbe6
William Maillard <william@zeero.ch>
2016-12-02 20:19:47 +0100

    uzfhgjfhuj

--- app/view/showFilm.php
+++ app/view/showFilm.php
@@ -1,11 +1,13 @@
 <style>
 	.showFilm {
 		width: 100%;
-		height: 100%;
+	}
+	.showFilm-top {
+		min-height: 370px;
 	}
 	.showFilm img {
-		max-width: 300px;
-		width: 90%;
+		width: 300px;
+		height: px;
 		float: left;
 		margin-right: 30px;
 		margin-bottom: 30px;
@@ -15,35 +17,78 @@
 		margin-top: 30px;
 		margin-bottom: 30px;
 	}
-	.commentaire {
-		padding: 2px;
-		border: 1px solid black;
-		margin-bottom: 3px;
+	.showFilm-price {
+		text-align: center;
 	}
-	.commentaire-top {
-		border-bottom: 1px solid black;
+	.commentaires {
+		width: 75%;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	.commentaire {
+		/*padding: 0 10px;
+		border: 1px solid black;*/
+		margin-bottom: 3px;
+		display: block;
+		width: 100%;
+		min-height: 100px;
+		/*background-color: white;*/
+	}
+	.commentaire-left, .commentaire-right {
+
+	}
+	.commentaire-left {
+		width: 39%;
+		float: left;
+	}
+	.commentaire-text {
+		text-align: justify;
+	}
+	.commentaire-right {
+		width: 59%;
+		float: right;
 	}
 </style>
 <div class="showFilm">
-	<img src="img/noimage.jpg" alt="cover" />
-	<h2><?php echo $film->getTitreTraduit(); ?></h2>
-	<h3><?php echo $film->getTitreOriginal(); ?></h3>
-	<p>DVD : <?php echo $film->getDvdPrice(); ?> CHF <button>+ Ajouter au panier</button></p>
-	<p>Blu-Ray : <?php echo $film->getBdPrice(); ?> CHF <button>+ Ajouter au panier</button></p>
-	<p style="text-align: justify;"><?php echo $film->getDescription(); ?></p>
+	<div class="showFilm-top">
+		<img src="img/noimage.jpg" alt="cover" />
+		<h2><?php echo $film->getTitreTraduit(); ?></h2>
+		<h3><?php echo $film->getTitreOriginal(); ?></h3>
+		<table class="showFilm-price">
+			<tr>
+				<td>DVD</td>
+				<td><?php echo $film->getDvdPrice(); ?></td>
+				<td>CHF</td>
+				<td><button>+ Ajouter au panier</button></td>
+			</tr>
+			<tr>
+				<td>Blu-Ray</td>
+				<td><?php echo $film->getBdPrice(); ?></td>
+				<td>CHF</td>
+				<td><button>+ Ajouter au panier</button></td>
+			</tr>
+		</table>
+		<p style="text-align: justify;"><?php echo $film->getDescription(); ?></p>
+	</div>
+	<br />
+	<hr />
 	<div class="showFilm-youtube">
+		<h3>Bande d'annonce</h3>
 		<?php $youtube->show(); ?>
 	</div>
 </div>
 <hr />
 <div class="commentaires">
-	<h3>Commentaires clients</h3>
+	<h3>Commentaires des clients</h3>
 	<?php while ($comments->fetch()) { ?>
+	<hr />
 	<div class="commentaire">
-		<p class="commentaire-top"><?php echo $comments->getName(); ?> [<?php echo $comments->getUsername(); ?>] // <?php echo $comments->getDateTime(); ?></p>
-		<p>
-			<?php echo $comments->getComment(); ?>
-		</p>
+		<div class="commentaire-left">
+			<p><?php echo $comments->getName(); ?> <!--[<?php echo $comments->getUsername(); ?>]--></p>
+			<p><?php echo $comments->getDateTime(); ?></p>
+		</div>
+		<div class="commentaire-right">
+			<div class="commentaire-text"><?php for($i = 0; $i < 10; $i++) { echo $comments->getComment() . " "; } ?></div>
 	</div>
 	<?php } ?>
 </div>
