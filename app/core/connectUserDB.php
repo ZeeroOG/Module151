7378ecf3526ae527745b899131bd9b63c7aafd27
William Maillard <william.djallel.maillard@gmail.com>
2016-12-21 22:18:21 +0100

    [WIP] Support *nix pour Access

--- app/core/connectUserDB.php
+++ app/core/connectUserDB.php
@@ -3,8 +3,21 @@
 function connectUserDB() {
 	// Chemin de la base de données (relatif au dossier du projet)
 	// puis check sur l'existance du fichier
-	if(strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') $path = PROJECT_PATH . "\db\users.mdb";
-	else $path = PROJECT_PATH . "/db/users.mdb";
+	if(strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') {
+		/*
+			Si Windaube :
+		*/
+
+		$diver = "{Microsoft Access Driver (*.mdb)}";
+		$path = PROJECT_PATH . "\db\users.mdb";
+	} else {
+		/*
+			Si Unix :
+			(Installer unixodbc + mdbtools)
+		*/
+		$driver = "MDBTools";
+		$path = PROJECT_PATH . "/db/users.mdb";
+	}
 
 	if(!file_exists($path)) {
 		echo "Erreur critique ! Vérifiez les logs.";
@@ -15,7 +28,7 @@ function connectUserDB() {
 	try {
 		// Création de l'objet PDO avec le driver ODBC
 		// Les strings de connexion sont disponibles ici : https://www.connectionstrings.com/microsoft-access-accdb-odbc-driver/
-		$database = new PDO("odbc:Driver={Microsoft Access Driver (*.mdb)};Dbq=$path;Uid=Admin;Pwd=;");
+		$database = new PDO("odbc:Driver=$driver;Dbq=$path;Uid=Admin;Pwd=;");
 	} catch(Exception $e) {
 		echo "Erreur critique ! Vérifiez les logs.";
 		Log::error("Echec de connexion Access 2002: " . $e->getMessage());
